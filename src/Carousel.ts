3f5005f2ce749bf1e37dfd0bc733360e4f0b8ba0
Mikael Arvola <marvel@illinois.edu>
2024-11-23 18:22:24 -0600

    Implementation for accessibility.

--- src/Carousel.ts
+++ src/Carousel.ts
@@ -1,79 +1,410 @@
-import { LitElement, html, unsafeCSS } from "lit";
+import {LitElement, html, unsafeCSS, TemplateResult, nothing, PropertyValues} from "lit";
 // @ts-ignore
-import styles from './Carousel.styles.css?inline';
+import styles from "./Carousel.styles.css?inline";
 // @ts-ignore
 import "./ilw-carousel.css";
-import { customElement, property, query, queryAssignedElements } from "lit/decorators.js";
+import { customElement, property, query, queryAll, queryAssignedElements, state } from "lit/decorators.js";
 import Slide from "./Slide";
+import { ManualSlotController } from "./util/ManualSlotController";
+import { classMap } from "lit/directives/class-map.js";
+import "@illinois-toolkit/ilw-icon";
 
-@customElement('ilw-carousel')
+@customElement("ilw-carousel")
 export default class Carousel extends LitElement {
-
-    @property()
-    theme = '';
-
-    @property()
-    label = '';
-
-    @property({reflect: true, type: Boolean})
-    playing = true;
-
-    @property({state: true})
-    private slideCount = 0;
-
-    @queryAssignedElements({selector: "ilw-slide"})
-    slidesSlot!: Array<Slide>;
+    static shadowRootOptions: ShadowRootInit = { ...LitElement.shadowRootOptions, slotAssignment: "manual" };
 
     static get styles() {
         return unsafeCSS(styles);
     }
 
+    @property()
+    theme = "";
+
+    /**
+     * Label for the whole carousel.
+     */
+    @property()
+    label = "";
+
+    /**
+     * Control play/pause state on the carousel. Defaults to false.
+     *
+     * This value is updated on the component, so it can be used for reading as well.
+     */
+    @property({ reflect: true, type: Boolean })
+    playing = true;
+
+    /**
+     * Time in seconds to stay on each slide.
+     */
+    @property({ type: Number })
+    time = 10;
+
+    @state()
+    private slideCount = 0;
+
+    /**
+     * 1-based active slide
+     *
+     * @private
+     */
+    @state()
+    private activeSlide = 1;
+
+    // Not in state, since this is used in the animation.
+    private hasHover = false;
+    private userStarted = false;
+    private hasControlFocus = false;
+    private hasTabFocus = false;
+
+    // This is not reactive, we only use it in render when redrawing the whole component,
+    // the style is normally updated through the timer.
+    private timer = 0;
+    private updateTime = 0;
+    private animationRequest = 0;
+
+    private manual = new ManualSlotController(this);
+
+    @query("#progress")
+    private progress!: HTMLDivElement;
+
+    @query("#progress-wrap")
+    private progressWrapper!: HTMLDivElement;
+
+    @query("#play-pause")
+    private playPause!: HTMLButtonElement;
+
+    @queryAll("[role=tab]")
+    private tabs!: Array<HTMLButtonElement>;
+
+    @query("#carousel-items")
+    private carouselItems!: HTMLDivElement;
+
     constructor() {
         super();
     }
 
-    private onSlotChange() {
-        let count = this.slidesSlot.length;
-        let i = 0;
-        for (let it of this.slidesSlot) {
-            it.slideCount = count;
-            it.slidePosition = i++;
+    public togglePlay() {
+        this.playing = !this.playing;
+        if (this.playing) {
+            this.userStarted = true;
+        }
+    }
+
+    /**
+     * Advance the carousel to the next slide.
+     */
+    public next() {
+        this.activeSlide = this.activeSlide === this.children.length ? 1 : this.activeSlide + 1;
+    }
+
+    /**
+     * Back the carousel to the previous slide.
+     */
+    public previous() {
+        this.activeSlide = this.activeSlide === 1 ? this.children.length : this.activeSlide - 1;
+    }
+
+    /**
+     * Set the active slide.
+     *
+     * @param slide 1-based index for the new active slide
+     */
+    public activateSlide(slide: number) {
+        if (slide < 1 || slide > this.children.length) {
+            throw new Error("ilw-carousel: attempted to set invalid slide: " + slide);
+        }
+        this.activeSlide = slide;
+        this.playing = false;
+    }
+
+    protected determinePrevNext(count: number, active: number) {
+        if (count < 2) {
+            // If we only have 1 slide, there's no prev or next
+            return {
+                prev: 0,
+                next: 0,
+            };
+        }
+        if (count === 2) {
+            // If active is 1, this is 2. If active is 2, this is 1.
+            return {
+                prev: 3 - active,
+                next: 3 - active,
+            };
+        }
+        // Loop around if active is the first or last
+        return {
+            prev: active === 1 ? count : active - 1,
+            next: active === count ? 1 : active + 1,
+        };
+    }
+
+    /**
+     * Go to next slide and stop playback due to interaction.
+     */
+    protected nextClick() {
+        this.playing = false;
+        this.next();
+    }
+
+    /**
+     * Go to previous slide and stop playback due to interaction.
+     */
+    protected previousClick() {
+        this.playing = false;
+        this.previous();
+    }
+
+    /**
+     * Stop slide's from advancing when mouse is over the carousel.
+     */
+    protected mouseOver(event: MouseEvent) {
+        if (event.target instanceof Node && this.playPause.contains(event.target)) {
+            // If the mouse is in the play button, we don't have to stop it since the user likely
+            // just clicked it
+            return;
+        }
+        this.hasHover = true;
+    }
+
+    protected mouseOut() {
+        this.hasHover = false;
+    }
+
+    /**
+     * When the slide selectors are focused, make the carousel a live region.
+     */
+    protected onTabFocus() {
+        this.hasTabFocus = true;
+        this.carouselItems.setAttribute("aria-live", "polite");
+    }
+
+    protected offTabFocus() {
+        this.hasTabFocus = false;
+        this.carouselItems.setAttribute("aria-live", "off");
+    }
+
+    /**
+     * Focus on the next and previous controls will pause the slides.
+     */
+    protected onControlFocus = () => {
+        this.hasControlFocus = true;
+    }
+
+    protected offControlFocus = () => {
+        this.hasControlFocus = false;
+    }
+
+    protected tabsKeyDown(event: KeyboardEvent) {
+        let flag = false;
+
+        switch (event.key) {
+            case "ArrowRight":
+                this.next();
+                flag = true;
+                break;
+
+            case "ArrowLeft":
+                this.previous();
+                flag = true;
+                break;
+
+            case "Home":
+                this.activateSlide(1);
+                flag = true;
+                break;
+
+            case "End":
+                this.activateSlide(this.children.length);
+                flag = true;
+                break;
+
+            default:
+                break;
+        }
+
+        if (flag) {
+            this.tabs[this.activeSlide - 1].focus();
+            event.stopPropagation();
+            event.preventDefault();
+        }
+    }
+
+    protected animationRequestCallback = () => {
+        if (this.playing) {
+            const now = Date.now();
+            const timeMs = this.time * 1000;
+
+            if (this.userStarted || (!this.hasHover && !this.hasControlFocus && !this.hasTabFocus)) {
+                this.timer += now - this.updateTime;
+
+                if (this.timer >= timeMs) {
+                    this.next();
+                    this.timer = 0;
+                }
+                this.updateTime = now;
+                if (this.progress) {
+                    this.progress.style.width = (this.progressWrapper.clientWidth * this.timer) / timeMs + "px";
+                }
+            } else {
+                this.updateTime = now;
+            }
+
+            requestAnimationFrame(this.animationRequestCallback);
+        } else if (this.animationRequest) {
+            cancelAnimationFrame(this.animationRequest);
+        }
+    };
+
+    connectedCallback() {
+        super.connectedCallback();
+        let hasReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)");
+        if (hasReducedMotion.matches) {
+            this.playing = false;
+        }
+        if (this.playing) {
+            this.animationRequest = requestAnimationFrame(this.animationRequestCallback);
+        }
+    }
+
+    protected update(changedProperties: PropertyValues) {
+        super.update(changedProperties);
+
+        if (changedProperties.has("playing") && this.playing) {
+            if (this.animationRequest) {
+                cancelAnimationFrame(this.animationRequest);
+            }
+            this.updateTime = Date.now();
+            this.animationRequest = requestAnimationFrame(this.animationRequestCallback);
+        }
+    }
+
+    protected updated(changedProperties: PropertyValues) {
+        super.updated(changedProperties);
+
+        // Listen to focus events on links inside the carousel content so we can pause it
+        for (let it of Array.from(this.querySelectorAll("ilw-slide a"))) {
+            it.addEventListener("focus", this.onControlFocus);
+            it.addEventListener("focusout", this.offControlFocus);
         }
     }
 
     render() {
+        let count = this.children.length;
+        const tabs: TemplateResult[] = [];
+        const items: TemplateResult[] = [];
+        const active = this.activeSlide; // activeSlide is 1-based
+        const { prev, next } = this.determinePrevNext(count, active);
+
+        for (let i = 1; i <= count; i++) {
+            let activateSlide = () => this.activateSlide(i);
+            tabs.push(
+                html` <button
+                    id="tab-${i}"
+                    type="button"
+                    role="tab"
+                    aria-label="Slide ${i}"
+                    aria-controls="slide-${i}"
+                    aria-selected=${i === active}
+                    tabindex=${i === active ? nothing : "-1"}
+                    @click=${activateSlide}
+                    @focusin=${this.onTabFocus}
+                    @focusout=${this.offTabFocus}
+                    @keydown=${this.tabsKeyDown}
+                >
+                    <span aria-hidden="true">${i}</span>
+                </button>`,
+            );
+            let classes = {
+                slide: true,
+                active: i === active,
+                prev: i === prev,
+                next: i === next,
+            };
+            items.push(
+                html` <div
+                    id="slide-${i}"
+                    class=${classMap(classes)}
+                    role="tabpanel"
+                    aria-roledescription="slide"
+                    aria-label=${`${i} of ${count}`}
+                >
+                    <slot></slot>
+                </div>`,
+            );
+        }
+
+        if (items.length < 2) {
+            return this.renderOne(items);
+        }
+
         return html`
-            <section class="carousel-inner">
-                <div class="play-control">
-                    <button type="button" class="play-pause"
-                    aria-label=${this.playing ? "Stop slides" : "Start slides"}>
-                        ${this.playing ? "⏸" : "⏵"}
+            <section class="carousel-inner" @mouseover=${this.mouseOver} @mouseout=${this.mouseOut}>
+                <div class="control-position">
+                    <div class="control">
+                        <div id="progress-wrap">
+                            <div id="progress"></div>
+                        </div>
+                        <div class="control-inner">
+                            <button
+                                type="button"
+                                id="play-pause"
+                                aria-label=${this.playing ? "Stop slides" : "Start slides"}
+                                @click=${this.togglePlay}
+                                @focusin=${this.onTabFocus}
+                                @focusout=${this.offTabFocus}
+                            >
+                                ${this.playing ? "⏸" : "⏵"}
+                            </button>
+                            <div class="tabs" role="tablist" aria-label="Slide Selector">${tabs}</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="previous-wrap">
+                    <button
+                        type="button"
+                        class="previous"
+                        aria-label="Previous Slide"
+                        aria-controls="carousel-items"
+                        @click=${this.previousClick}
+                        @focusin=${this.onControlFocus}
+                        @focusout=${this.offControlFocus}
+                    >
+                        <ilw-icon icon="back" size="32px"></ilw-icon>
                     </button>
                 </div>
-                <div class="previous">
-                    <button type="button" class="previous"
-                            aria-label="Previous Slide"
-                            aria-controls="carousel-items">
-                        <ilw-icon icon="back"></ilw-icon>
+                <div class="next-wrap">
+                    <button
+                        type="button"
+                        class="next"
+                        aria-label="Next Slide"
+                        aria-controls="carousel-items"
+                        @click=${this.nextClick}
+                        @focusin=${this.onControlFocus}
+                        @focusout=${this.offControlFocus}
+                    >
+                        <ilw-icon icon="next" size="32px"></ilw-icon>
                     </button>
                 </div>
-                <div class="next">
-                    <button type="button" class="next"
-                            aria-label="Next Slide"
-                            aria-controls="carousel-items">
-                        <ilw-icon icon="back"></ilw-icon>
-                    </button>
-                </div>
-                <div id="carousel-items" aria-live="off">
-                    <slot @slotchange=${this.onSlotChange}></slot>
-                </div>
+                <div id="carousel-items" aria-live="${this.hasTabFocus ? "polite" : "off"}">${items}</div>
             </section>
+        `;
+    }
+
+    protected renderOne(items: TemplateResult[]) {
+        return html`
+            <div class="carousel-inner-single">
+                <div id="carousel-items">
+                    <slot></slot>
+                </div>
+            </div>
         `;
     }
 }
 
 declare global {
     interface HTMLElementTagNameMap {
-        'ilw-carousel': Carousel
+        "ilw-carousel": Carousel;
     }
 }
--- src/Slide.ts
+++ src/Slide.ts
@@ -9,12 +9,6 @@ export default class Slide extends LitElement {
     @property()
     theme = '';
 
-    @property({type: Number})
-    slideCount = 0;
-
-    @property({type: Number})
-    slidePosition = 0;
-
     static get styles() {
         return unsafeCSS(styles);
     }
@@ -24,14 +18,13 @@ export default class Slide extends LitElement {
     }
 
     render() {
-        let label = `${this.slidePosition + 1} of ${this.slideCount}`
         return html`
-            <div class="slide" role="group" aria-roledescription="slide" aria-label=${label}>
+            <div class="slide-inner">
                 <div class="image">
                     <slot name="image"></slot>
                 </div>
                 <div class="content">
-                    <slot></slot>
+                    <slot name="content"></slot>
                 </div>
             </div>
         `;
